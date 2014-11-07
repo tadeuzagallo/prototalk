c0d125987200ba06a10d5a3eaf69f888246aac14
Tadeu Zagallo <tadeuzagallo@gmail.com>
2014-11-07 14:47:12 -0200

    Add yaml configuration files

--- flo.js
+++ flo.js
@@ -1,16 +1,31 @@
+var exec = require('child_process').exec;
 var flo = require('fb-flo');
-var path = require('path');
 var fs = require('fs');
 var jade = require('jade');
-var exec = require('child_process').exec;
+var path = require('path');
+var yaml = require('yamljs');
 
 var dir = './src/';
 var port = 8888;
 
 var server = flo(dir, {
   port: port,
-  glob: ['js/*.js', 'css/*.css', '*.jade']
+  glob: ['js/*.js', 'css/*.css', '*.jade', '*.yml']
 }, resolver);
+
+function parseYaml(file) {
+  var filepath = path.join(dir, file);
+  return yaml.load(filepath);
+}
+
+function loadConfig() {
+  var config = parseYaml('config.yml');
+  config.slides = parseYaml('slides.yml');
+
+  return config;
+}
+
+var config = loadConfig();
 
 server.once('ready', function () {
   console.log('Server listening on port %s', port);
@@ -22,29 +37,26 @@ server.on('request', function () {
 
 function resolver(filepath, callback) {
   var ext = path.extname(filepath);
-  var absPath = path.join(dir, filepath);
-  var url;
-  var contents;
 
-  console.log(filepath);
-  if (ext === '.jade') {
-    exec('jade ' + absPath, function (err) {
-      if (err) {
-        console.log(err);
-      } else {
-        var fp = filepath.replace('.jade', '.html');
-        callback({
-          resourceURL: fp,
-          reload: true,
-          contents: fs.readFileSync(path.join(dir, fp))
-        });
-      }
-    });
-  } else {
-    callback({
-      resourceURL: filepath,
-      contents: fs.readFileSync(path.join(dir, filepath))
-    });
+  console.log('Refreshing %s', filepath);
+
+  switch (ext) {
+    case '.yml':
+      config = loadConfig();
+    case '.jade':
+      var fp = path.join(dir, 'index.jade');
+      var html = jade.renderFile(fp, config);
+      fs.writeFileSync(path.join(dir, 'index.html'), html);
+      callback({
+        resourceURL: fp,
+        reload: true,
+        contents: html
+      });
+      break;
+    default:
+      callback({
+        resourceURL: filepath,
+        contents: fs.readFileSync(path.join(dir, filepath))
+      });
   }
-
 }
--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,13 @@
 Reveal.initialize({
   loop: false,
   transition: 'linear',
-  brackgoundTransition: 'slide'
+  brackgoundTransition: 'slide',
+  
+  dependencies: [
+    { src: 'components/reveal.js/lib/js/classList.js', condition: function() { return !document.body.classList; } },
+    { src: 'components/reveal.js/plugin/markdown/marked.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
+    { src: 'components/reveal.js/plugin/highlight/highlight.js', async: true, callback: function() { hljs.initHighlightingOnLoad(); } },
+    { src: 'components/reveal.js/plugin/zoom-js/zoom.js', async: true, condition: function() { return !!document.body.classList; } },
+    { src: 'components/reveal.js/plugin/notes/notes.js', async: true, condition: function() { return !!document.body.classList; } },
+  ]
 });
